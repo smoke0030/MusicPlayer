12a172de101375f996b7f06433a9e7746da661f4
Sergey Vidishev <ft.priest@gmail.com>
2024-08-09 10:36:36 +0300

    Initial Commit

---
+++ MusicPlayer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MusicPlayer
+//
+//  Created by Сергей on 09.08.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MusicPlayer/MusicPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicPlayerApp.swift
+//  MusicPlayer
+//
+//  Created by Сергей on 09.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct MusicPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
